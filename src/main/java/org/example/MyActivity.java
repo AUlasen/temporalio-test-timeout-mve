ca1e1ffa144f4ea64c96fe26a83c8ec5852faab8
AndreiUlasen <a.ulasen@gmail.com>
2025-07-27 13:20:25 +0200

    Remove explicit policy

--- src/main/java/org/example/MyActivity.java
+++ src/main/java/org/example/MyActivity.java
@@ -2,7 +2,6 @@ package org.example;
 
 import io.temporal.activity.ActivityInterface;
 import io.temporal.activity.ActivityOptions;
-import io.temporal.common.RetryOptions;
 import io.temporal.workflow.Workflow;
 
 import java.time.Duration;
@@ -12,7 +11,7 @@ public interface MyActivity {
     static MyActivity getStub() {
         return Workflow.newActivityStub(
                 MyActivity.class,
-                ActivityOptions.newBuilder().setScheduleToCloseTimeout(Duration.ofSeconds(30)).setRetryOptions(RetryOptions.newBuilder().setMaximumAttempts(0).setBackoffCoefficient(1).setInitialInterval(Duration.ofSeconds(5)).build()).build());
+                ActivityOptions.newBuilder().setScheduleToCloseTimeout(Duration.ofSeconds(30)).build());
     }
 
     void executeActivity();
